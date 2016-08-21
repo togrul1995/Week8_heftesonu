9c1feaf136f3045be735b7cc6a74a37bebd63e84
Togrul Yariyev <togrulyariyev@gmail.com>
2016-08-21 19:27:11 +0400

    Some problems emerged

---
+++ index_week8_heftesonu.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <?php
+                echo date('d-M-y G:i:s');
+                ?>
+                <div class="col-md-6 col-md-offset-3">
+                    <h3 class="title">Qeydiyyat</h3>
+                    <form method="post" action="upload_run.php" enctype="multipart/form-data">
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="name" placeholder="Adı">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="place" placeholder="Yer adı">
+                        </div>
+                        <div class="form-group">
+                            <input type="file" class="form-control" name="sekil">
+                        </div>
+                        <div class="form-group">
+                            <input class="btn btn-default pull-right" type="submit" value="Yukle" name="upload">
+                        </div>
+                    </form>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-md-12">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>Yükləyən</th>
+                                    <th>Yer</th>
+                                    <th>Şəkil</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Yolchu Nasib</td>
+                                    <td>Quba</td>
+                                    <td>
+                                        <img width="128" src="uploads/19082016152158photo.jpg" class="img-responsive">
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
